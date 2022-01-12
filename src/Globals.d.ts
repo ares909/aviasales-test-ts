7faf831a9ea2f67529e034b4b5c0f6dc71fda40e
Dmitriy Khomyakov <ares909@yandex.ru>
2022-01-12 12:24:26 +0300

    Updates

--- src/Globals.d.ts
+++
@@ -1,6 +0,0 @@
-declare module "*.module.css";
-declare module "*.module.scss";
-declare module "*.svg" {
-    const value: string;
-    export default value;
-}
--- src/utils/filter.ts
+++
@@ -1,7 +0,0 @@
-const filterTickets = (array: any[], stops: any[]) => {
-    return array.filter(
-        (item) => stops.includes(item.segments[0].stops.length) && stops.includes(item.segments[1].stops.length),
-    );
-};
-
-export default filterTickets;
